0879379fafbe65392d0e8966c3be42476cb45928
ACHRAL <achreflaayounii@gmail.com>
2023-12-29 17:00:44 +0100

    Departement

--- src/main/java/com/csi/rh_project/setup/model/Department.java
+++ src/main/java/com/csi/rh_project/setup/model/Department.java
@@ -1,6 +1,10 @@
 package com.csi.rh_project.setup.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @jakarta.persistence.Entity
 @Table(name = "departments")
